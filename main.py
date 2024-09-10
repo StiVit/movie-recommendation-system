771bdeebb51ca075aaf2c62acef3ad6cbcb3ecd4
vitalie stinca <vitalicstinca123@gmail.com>
2024-09-10 19:54:19 +0300

    feat: Analyse the data based on number of null values and repeated values in the df

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.proccess_csv import process_data
+
+if __name__ == "__main__":
+    process_data()
--- src/proccess_csv.py
+++ src/proccess_csv.py
@@ -2,10 +2,22 @@ import numpy as np
 import matplotlib
 import pandas as pd
 import logging
+import seaborn as sns
 from utils.logger_setup import setup_logger
 
 
-movies = pd.read_csv('../dataset/tmdb_5000_movies.csv')
-app_logger = setup_logger("app_logger", logging.INFO)
+def process_data():
+    df_movies = pd.read_csv('dataset/tmdb_5000_movies.csv')
+    app_logger = setup_logger("app_logger", logging.INFO)
 
-print(movies.info())
+    # Get some more info about the data I'm working with
+    with open('dataset/data_frame_analysis', 'w') as output:
+        output.write(f"Movies DF:\n\n{df_movies.head()}\n")
+        output.write("\n\nMovies DF:\n")
+        output.write(f"{df_movies.info()}\n\n")
+        output.write(f"{df_movies.describe()}\n\n")
+        output.write(f"{df_movies['original_language'].value_counts()}")
+        output.write(f"\n{df_movies['status'].value_counts()}")
+        output.write(f"\n\nDuplicates:\n\n{df_movies.duplicated().sum()}")
+        output.write(f"\n\nNulls:\n\n{df_movies.isnull().sum()}\n\n**************************************\n\n")
+    app_logger.info("Data analysed successfully")
