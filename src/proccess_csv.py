ba149db98e7b12a26975dcbd85df22fd68bc98ba
vitalie stinca <vitalicstinca123@gmail.com>
2024-09-11 16:46:12 +0300

    feat: Write the function to extract names and language from data

--- src/proccess_csv.py
+++ src/proccess_csv.py
@@ -1,10 +1,36 @@
 import numpy as np
-import matplotlib
 import pandas as pd
 import logging
+import re
 import seaborn as sns
+from matplotlib import pyplot as plt
+
 from utils.logger_setup import setup_logger
 import os
+
+patternname = r"(?:.name.: .)(\w{1,}\s{0,}\w{0,})"
+patternlang = r"(?:.iso_639_1.: .)(\w{1,}\s{0,}\w{0,})"
+
+def dict2list(x):
+    """
+    Extract important info out the string columns that contain of lists of dictionaries
+    """
+    if type(x) is str:
+        templist = x.strip('[]').split(',')
+        namelist = []
+        lang = False
+        for element in templist:
+            if re.search(patternlang, element):
+                namelist.append(re.search(patternlang, element).group(0)[14:])
+                lang = True
+            elif re.search(patternname, element):
+                namelist.append(re.search(patternname, element).group(0)[9:])
+        if len(namelist) > 0:
+            return {k: 1 for k in namelist}
+        else:
+            return x
+    else:
+        return x
 
 
 def process_data():
@@ -24,4 +50,8 @@ def process_data():
             output.write(f"\n{df_movies['status'].value_counts()}")
             output.write(f"\n\nDuplicates:\n\n{df_movies.duplicated().sum()}")
             output.write(f"\n\nNulls:\n\n{df_movies.isnull().sum()}\n\n**************************************\n\n")
+            df_movies.hist(figsize=(20, 12), bins=100)
+            plt.show()
         app_logger.info("Data analysed successfully")
+
+    df_movies = df_movies.applymap(dict2list)
